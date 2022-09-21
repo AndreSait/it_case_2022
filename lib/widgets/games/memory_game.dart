4ff2c58844427e45979b9f895fd679614c70e30d
sondresorbye0111 <sondre@magson.no>
2022-09-21 10:29:56 +0200

    feat: fixed animation and logic for memory game
    Co-authored-by: Jonah Wiecek <jonahwiecek@gmail.com>

--- lib/widgets/games/memory_game.dart
+++ lib/widgets/games/memory_game.dart
@@ -1,56 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:it_case_2022/generate_person_list.dart';
+import 'package:it_case_2022/models/person.dart';
 import '../../game_manager.dart';
 import "../game_components/memory_game/flip_card.dart";
 
 class MemoryGame extends StatefulWidget {
   static const routeName = "memory-game";
   final GameManager gameManager;
-  const MemoryGame(this.gameManager, {super.key});
+  final List<Person> persons;
+  final List<Person> selectedPersons;
+  MemoryGame(this.gameManager, this.persons, {super.key})
+      : selectedPersons = generatePersonList(persons, 4);
 
   @override
   State<MemoryGame> createState() => _MemoryGameState();
 }
 
 class _MemoryGameState extends State<MemoryGame> {
+  Person? selectedPerson;
+  late List<CardInfo> cards;
+  int score = 0;
+  bool gameOver = false;
+
+  @override
+  void initState() {
+    cards = [];
+    widget.selectedPersons.forEach((element) {
+      cards.add(CardInfo(
+          person: element,
+          showImage: true,
+          isFlipped: false,
+          isMatched: false));
+      cards.add(CardInfo(
+          person: element,
+          showImage: false,
+          isFlipped: false,
+          isMatched: false));
+    });
+    cards.shuffle();
+    super.initState();
+  }
+
+  void personCardClicked(CardInfo card) {
+    setState(() {
+      if (!card.isFlipped) {
+        card.isFlipped = true;
+
+        if (selectedPerson == null) {
+          selectedPerson = card.person;
+        } else {
+          if (selectedPerson == card.person) {
+            cards.forEach((element) {
+              if (element.person == card.person && element.isFlipped) {
+                element.isMatched = true;
+              }
+            });
+            selectedPerson = null;
+          } else {
+            cards.forEach((element) {
+              if (element.person == card.person &&
+                  element.showImage == card.showImage) {
+                element.isFlipped = true;
+              }
+              if (element.person == selectedPerson) {
+                element.isFlipped = false;
+              }
+            });
+            selectedPerson = card.person;
+          }
+        }
+        // if all cards are matched, set gameOver to true
+        if (cards.every((element) => element.isMatched)) {
+          gameOver = true;
+        }
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // final routeArgs =
-    //     ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
-    // final categoryTitle = routeArgs["title"];
-    // final categoryId = routeArgs["id"];
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Memory Game"),
-            Container(
-                width: double.infinity,
-                height: 450,
-                child: GridView(
-                  padding: const EdgeInsets.all(25),
-                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 3 / 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
-                  ),
-                  children: [
-                    FlipCard(),
-                    FlipCard(),
-                    FlipCard(),
-                    FlipCard(),
-                    FlipCard(),
-                    FlipCard(),
-                  ],
-                )),
-            ElevatedButton(
-              child: Text("Next"),
-              onPressed: () => widget.gameManager.nextGame(context, 100),
-            ),
-          ],
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: GridView(
+                padding: const EdgeInsets.all(25),
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 150,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                ),
+                children:
+                    cards.map((c) => FlipCard(c, personCardClicked)).toList(),
+              )),
+              gameOver
+                  ? ElevatedButton(
+                      child: Text("Next"),
+                      onPressed: () =>
+                          widget.gameManager.nextGame(context, 100),
+                    )
+                  : Container(),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class CardInfo {
+  final Person person;
+  bool showImage;
+  bool isFlipped;
+  bool isMatched;
+  CardInfo(
+      {required this.person,
+      required this.showImage,
+      required this.isFlipped,
+      required this.isMatched});
 }
